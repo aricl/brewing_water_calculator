fd3bb20ea5b42538c2d26b7b239f2fbaa2707c4e
Adam John Ready <worldelephantseal@gmail.com>
2020-09-13 18:33:28 +0100

    Modified the water profile and AMS to not include pH or hardness. I know AMS affects pH, but I have not found concrete data on how. Same with hardness. Since the main numbers to worry about are chlorides, sulphates, sodium and alkalinity I'll stick to those

--- input_water_profile.py
+++ input_water_profile.py
@@ -3,18 +3,14 @@ import input_concentration as ic
 
 
 def input_water_profile():
-    ph = ic.input_concentration('pH: ')
     chloride_concentration = ic.input_concentration('Chloride (Cl, ppm): ')
     sulphate_concentration = ic.input_concentration('Sulphate (SO4, ppm): ')
-    total_hardness_as_carbonate_concentration = ic.input_concentration('Hardness as calcium carbonate (CaCO3, ppm): ')
     alkalinity_as_carbonate_concentration = ic.input_concentration('Alkalinity as calcium carbonate (CaCO3, ppm): ')
     sodium_concentration = ic.input_concentration('Sodium (Na, ppm): ')
 
     water_profile = np.array([
-        ph,
         chloride_concentration,
         sulphate_concentration,
-        total_hardness_as_carbonate_concentration,
         alkalinity_as_carbonate_concentration,
         sodium_concentration
     ])
