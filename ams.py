a071c0ec30b814650f144074d772a37015b5ff25
Adam John Ready <worldelephantseal@gmail.com>
2020-09-13 19:07:40 +0100

    Added a sodium_chloride.py set of concentrations

--- ams.py
+++ ams.py
@@ -5,6 +5,7 @@ def get_ams_concentrations():
     """
         Key: Ca, Cl, SO4, alkalinity, Na
         The effect of 1mL of AMS on 1 litre of liquor
+        All values have been rounded to the nearest ppm
     :return: np.array
     """
     return np.array([0, 65, 90, -185, 0])
--- sodium_chloride.py
+++ sodium_chloride.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def get_calcium_chloride_concentrations():
+def get_sodium_chloride_concentrations():
     """
         Key: Ca, Cl, SO4, alkalinity, Na
-        The effect of 1g of CaCl on 1 litre of liquor
+        The effect of 1g of NaCl on 1 litre of liquor
         All values have been rounded to the nearest ppm
     :return: np.array
     """
-    return np.array([160, 280, 0, 0, 0])
+    return np.array([0, 375, 0, 0, 246])
