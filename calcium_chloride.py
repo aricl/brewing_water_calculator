223c65d2fce0b63ee4fd22f5682e05b65f5734fb
Adam John Ready <worldelephantseal@gmail.com>
2020-09-13 18:43:13 +0100

    Renamed calcium_chloride.get_ams_concentrations() to get_calcium_chloride_concentrations()

--- calcium_chloride.py
+++ calcium_chloride.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def get_ams_concentrations():
+def get_calcium_chloride_concentrations():
     """
         Key: Ca, Cl, SO4, alkalinity, Na
         The effect of 1g of CaCl on 1 litre of liquor
