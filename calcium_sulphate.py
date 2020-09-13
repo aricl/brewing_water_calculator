37fe09bfde04a63e6c611174d52575d13222de56
Adam John Ready <worldelephantseal@gmail.com>
2020-09-13 19:10:46 +0100

    Added a calcium_sulphate.py set of concentrations

---
+++ calcium_sulphate.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def get_calcium_chloride_concentrations():
+    """
+        Key: Ca, Cl, SO4, alkalinity, Na
+        The effect of 1g of CaSO4 on 1 litre of liquor
+        All values have been rounded to the nearest ppm
+    :return: np.array
+    """
+    return np.array([218, 0, 524, 0, 0])
