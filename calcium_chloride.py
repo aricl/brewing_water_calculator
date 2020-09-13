95951ed26921e01aa70aba106b8c91a86398699e
Adam John Ready <worldelephantseal@gmail.com>
2020-09-13 18:38:52 +0100

    Modified the input_water_profile.py to include calcium. Also added a file calcium_chloride.py that records the contribution of CaCl to a water profile

--- calcium_chloride.py
+++ calcium_chloride.py
@@ -4,7 +4,7 @@ import numpy as np
 def get_ams_concentrations():
     """
         Key: Ca, Cl, SO4, alkalinity, Na
-        The effect of 1mL of AMS on 1 litre of liquor
+        The effect of 1g of CaCl on 1 litre of liquor
     :return: np.array
     """
-    return np.array([0, 65, 90, -185, 0])
+    return np.array([160, 280, 0, 0, 0])
--- input_water_profile.py
+++ input_water_profile.py
@@ -3,12 +3,14 @@ import input_concentration as ic
 
 
 def input_water_profile():
+    calcium_concentration = ic.input_concentration('Calcium (Ca, ppm): ')
     chloride_concentration = ic.input_concentration('Chloride (Cl, ppm): ')
     sulphate_concentration = ic.input_concentration('Sulphate (SO4, ppm): ')
     alkalinity_as_carbonate_concentration = ic.input_concentration('Alkalinity as calcium carbonate (CaCO3, ppm): ')
     sodium_concentration = ic.input_concentration('Sodium (Na, ppm): ')
 
     water_profile = np.array([
+        calcium_concentration,
         chloride_concentration,
         sulphate_concentration,
         alkalinity_as_carbonate_concentration,
