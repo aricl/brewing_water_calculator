144078de82cb14f37b7a5c5caa508cae0b5a82a6
Adam John Ready <worldelephantseal@gmail.com>
2020-09-14 21:24:16 +0100

    Renamed input_concentration.py to input_float

---
+++ input_float.py
@@ -0,0 +1,11 @@
+def input_float(input_prompt):
+    print(input_prompt, end='')
+    try:
+        user_input = input()
+        float_value = float(user_input)
+    except ValueError:
+        print('Please enter a number of parts per million, e.g. 123.45')
+        float_value = input_float(input_prompt)
+        return float_value
+    else:
+        return float_value
--- input_water_profile.py
+++ input_water_profile.py
@@ -1,14 +1,14 @@
 import numpy as np
-import input_concentration as ic
+import input_float as ic
 
 
 def input_water_profile():
-    calcium_concentration = ic.input_concentration('Calcium (Ca, ppm): ')
-    chloride_concentration = ic.input_concentration('Chloride (Cl, ppm): ')
-    sulphate_concentration = ic.input_concentration('Sulphate (SO4, ppm): ')
-    alkalinity_as_carbonate_concentration = ic.input_concentration('Alkalinity as calcium carbonate (CaCO3, ppm): ')
-    sodium_concentration = ic.input_concentration('Sodium (Na, ppm): ')
-    magnesium_concentration = ic.input_concentration('Magnesium (Mg, ppm): ')
+    calcium_concentration = ic.input_float('Calcium (Ca, ppm): ')
+    chloride_concentration = ic.input_float('Chloride (Cl, ppm): ')
+    sulphate_concentration = ic.input_float('Sulphate (SO4, ppm): ')
+    alkalinity_as_carbonate_concentration = ic.input_float('Alkalinity as calcium carbonate (CaCO3, ppm): ')
+    sodium_concentration = ic.input_float('Sodium (Na, ppm): ')
+    magnesium_concentration = ic.input_float('Magnesium (Mg, ppm): ')
 
     water_profile = np.array([
         calcium_concentration,
