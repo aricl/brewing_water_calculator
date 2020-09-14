dedeff6815fc7b93b2774be6eb4c63a27ebda8dc
Adam John Ready <worldelephantseal@gmail.com>
2020-09-14 23:27:43 +0100

    Tidied up main.py a bit

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import input_water_profile as iwp
 from additions_calculator import calculate_additions
 
 print('Please enter your initial water profile:')
-
 initial_water_profile = iwp.input_water_profile()
 
 print('Now please enter your target water profile:')
-
 target_water_profile = iwp.input_water_profile()
 
 result = calculate_additions(initial_water_profile, target_water_profile)
