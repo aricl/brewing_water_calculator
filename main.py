0fc79ed9f89899a8eda803d19c82f970ec6f4110
Adam John Ready <worldelephantseal@gmail.com>
2020-09-14 23:20:27 +0100

    Added a calculation in main.py that correctly calculates the additions needed (via a linear decomposition) for given initial and target water profiles

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import numpy as np
 
 import input_water_profile as iwp
+from additions.ams import get_ams_concentrations as ams
+from additions.calcium_chloride import get_calcium_chloride_concentrations as calcium_chloride
+from additions.calcium_sulphate import get_calcium_sulphate_concentrations as calcium_sulphate
+from additions.dwb import get_dwb_concentrations as dwb
+from additions.magnesium_sulphate import get_magnesium_sulphate_concentrations as magnesium_sulphate
+from additions.sodium_chloride import get_sodium_chloride_concentrations as sodium_chloride
 
 print('Please enter your initial water profile:')
 
@@ -12,3 +18,10 @@ target_water_profile = iwp.input_water_profile()
 
 water_profile_difference = np.subtract(target_water_profile, initial_water_profile)
 print(water_profile_difference)
+
+matrix = np.transpose(np.array([ams(), calcium_chloride(), calcium_sulphate(), dwb(), magnesium_sulphate(), sodium_chloride()]))
+
+result = np.linalg.solve(matrix, water_profile_difference)
+
+print(result)
+print(matrix.dot(np.linalg.solve(matrix, water_profile_difference)) - water_profile_difference)
