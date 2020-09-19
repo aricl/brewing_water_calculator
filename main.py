ecf48af39dfdeab2816f728024e1ec22177b8560
Adam John Ready <worldelephantseal@gmail.com>
2020-09-19 19:41:38 +0100

    Move the code inside main.py into the mode_selector_view.py file. This in turn calls the additions_calculator_view.py for calculating additions given an initial and final water profile

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import input_water_profile as iwp
-from additions_calculator import calculate_additions
+from views.mode_selector_view import view as mode_selector_view
 
-print('Please enter your initial water profile:')
-initial_water_profile = iwp.input_water_profile()
-
-print('Now please enter your target water profile:')
-target_water_profile = iwp.input_water_profile()
-
-result = calculate_additions(initial_water_profile, target_water_profile)
-
-print(result)
+mode_selector_view()
---
+++ views/additions_calculator_view.py
@@ -0,0 +1,14 @@
+import input_water_profile as iwp
+from additions_calculator import calculate_additions
+
+
+def view():
+    print('Please enter your initial water profile:')
+    initial_water_profile = iwp.input_water_profile()
+
+    print('Now please enter your target water profile:')
+    target_water_profile = iwp.input_water_profile()
+
+    result = calculate_additions(initial_water_profile, target_water_profile)
+
+    print(result)
