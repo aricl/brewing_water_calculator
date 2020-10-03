1a6801f19bbd8836ddd825c61dfd95dfc49becee
Adam John Ready <worldelephantseal@gmail.com>
2020-10-03 22:28:44 +0100

    Added the additions_calculator_test.py test_kernel_stouts_porters_water_profile test. Apparently the Kernel water profile differs from the one I'm calculating because it adds about 67 ppm of Na, which is how it gets so close to the expected profile, by ignoring Na altogether

--- tests/additions_calculator_test.py
+++ tests/additions_calculator_test.py
@@ -48,6 +48,28 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(expected_additions, calculated_additions)
 
+    def test_kernel_stouts_porters_water_profile(self):
+        # Ca, Cl, SO4, Alkalinity as CO3, Na, Mg
+        initial_water_profile = np.array([87.36, 43.74, 52.3, 190, 0.0, 5.09])
+        target_water_profile = np.array([134.36, 266.74, 99.30, 94.00, 0.0, 5.09])
+
+        calculated_additions = additions_calculator.calculate_additions(
+            initial_water_profile,
+            target_water_profile
+        )
+
+        expected_additions = {
+            'ams': '0.54mL/L',
+            'calcium_chloride': '0.29g/L',
+            'calcium_sulphate': '0.0g/L',
+            'dwb': '0.0g/L',
+            'magnesium_sulphate': '0.0g/L',
+            'sodium_chloride': '0.06g/L',
+            'lactic_acid': '0.0mL/L'
+        }
+
+        self.assertEqual(expected_additions, calculated_additions)
+
 
 if __name__ == '__main__':
     unittest.main()
