86e144eb9f279dd41e2f2883e384998bd4ba577d
Adam John Ready <worldelephantseal@gmail.com>
2020-10-04 12:22:03 +0100

    Corrected the lactic_acid.py doc-string for the get_concentrations method

--- additions/lactic_acid.py
+++ additions/lactic_acid.py
@@ -4,7 +4,7 @@ import numpy as np
 def get_concentrations():
     """
         Key: Ca, Cl, SO4, alkalinity, Na, Mg
-        The effect of 1mL of AMS on 1 litre of liquor
+        The effect of 1mL of lactic acid on 1 litre of liquor
         All values have been rounded to the nearest ppm
     :return: np.array
     """
