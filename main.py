6429f9b807e592ecaf18e70e1ea6e4c12369ecd8
Adam John Ready <worldelephantseal@gmail.com>
2020-10-12 22:08:00 +0100

    Started using Kivy to create an app User Interface. It's pretty early days!

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-from views.mode_selector_view import view as mode_selector_view
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
-mode_selector_view()
+
+class ButtonApp(App):
+    def build(self):
+        layout = BoxLayout(padding=10)
+
+        additions_from_profile_button = Button(text='Additions From Profile')
+        additions_from_profile_button.bind(on_press=self.on_button_press)
+        layout.add_widget(additions_from_profile_button)
+
+        profile_from_additions_button = Button(text='Profile From Additions')
+        profile_from_additions_button.bind(on_press=self.on_button_press)
+        layout.add_widget(profile_from_additions_button)
+
+        return layout
+
+    def on_button_press(self, instance):
+        print(instance.text)
+
+
+if __name__ == '__main__':
+    app = ButtonApp()
+    app.run()
