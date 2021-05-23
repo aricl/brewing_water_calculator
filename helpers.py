46b7f6aff0245d34d2e3a83d3ac05f73e6c477de
Adam John Ready <worldelephantseal@gmail.com>
2021-05-23 12:54:39 +0100

    Managed to create a simple kivy page that takes a water profile as input and then creates a dialog box containing the calculated water profile from a hard-coded set of additions

---
+++ helpers.py
@@ -0,0 +1,65 @@
+calcium_concentration_definition = '''
+MDTextField:
+    hint_text: "Ca:"
+    helper_text: "Enter the calcium concentration in parts per million (ppm)"
+    helper_text_mode: "on_focus"
+    pos_hint: {'center_x': 0.5, 'center_y': 0.8}
+    size_hint_x: None
+    size_hint_y: None
+    width: 400
+'''
+
+chloride_concentration_definition = '''
+MDTextField:
+    hint_text: "Cl:"
+    helper_text: "Enter the chloride concentration in parts per million (ppm)"
+    helper_text_mode: "on_focus"
+    pos_hint: {'center_x': 0.5, 'center_y': 0.7}
+    size_hint_x: None
+    size_hint_y: None
+    width: 400
+'''
+
+sulphate_concentration_definition = '''
+MDTextField:
+    hint_text: "SO4:"
+    helper_text: "Enter the sulphate concentration in parts per million (ppm)"
+    helper_text_mode: "on_focus"
+    pos_hint: {'center_x': 0.5, 'center_y': 0.6}
+    size_hint_x: None
+    size_hint_y: None
+    width: 400
+'''
+
+alkalinity_as_carbonate_concentration_definition = '''
+MDTextField:
+    hint_text: "Alkalinity as CO3:"
+    helper_text: "Enter the alkalinity as carbonate concentration in parts per million (ppm)"
+    helper_text_mode: "on_focus"
+    pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+    size_hint_x: None
+    size_hint_y: None
+    width: 400
+'''
+
+sodium_concentration_definition = '''
+MDTextField:
+    hint_text: "Na:"
+    helper_text: "Enter the sodium concentration in parts per million (ppm)"
+    helper_text_mode: "on_focus"
+    pos_hint: {'center_x': 0.5, 'center_y': 0.4}
+    size_hint_x: None
+    size_hint_y: None
+    width: 400
+'''
+
+magnesium_concentration_definition = '''
+MDTextField:
+    hint_text: "Mg:"
+    helper_text: "Enter the magnesium concentration in parts per million (ppm)"
+    helper_text_mode: "on_focus"
+    pos_hint: {'center_x': 0.5, 'center_y': 0.3}
+    size_hint_x: None
+    size_hint_y: None
+    width: 400
+'''
--- main.py
+++ main.py
@@ -1,40 +1,80 @@
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import Screen
+from kivymd.uix.button import MDRectangleFlatButton, MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivy.lang.builder import Builder
+import helpers
+import numpy as np
+
+from profile_calculator import calculate_profile
 
 
-Builder.load_file('screens/menu.kv')
-Builder.load_file('screens/additions_calculator.kv')
-Builder.load_file('screens/profile_calculator.kv')
-
-
-class Menu(Screen):
-    pass
-
-
-class AdditionsCalculator(Screen):
-    pass
-
-
-class ProfileCalculator(Screen):
-    pass
-
-
-screen_manager = ScreenManager()
-screen_manager.add_widget(Menu(name='menu'))
-screen_manager.add_widget(AdditionsCalculator(name='additions_calculator'))
-screen_manager.add_widget(ProfileCalculator(name='profile_calculator'))
-
-
-class TestApp(App):
-
+class DemoApp(MDApp):
     def build(self):
-        return screen_manager
+        screen = Screen()
+        self.calcium_concentration = Builder.load_string(helpers.calcium_concentration_definition)
+        self.chloride_concentration = Builder.load_string(helpers.chloride_concentration_definition)
+        self.sulphate_concentration = Builder.load_string(helpers.sulphate_concentration_definition)
+        self.alkalinity_as_carbonate_concentration = Builder.load_string(helpers.alkalinity_as_carbonate_concentration_definition)
+        self.sodium_concentration = Builder.load_string(helpers.sodium_concentration_definition)
+        self.magnesium_concentration = Builder.load_string(helpers.magnesium_concentration_definition)
+        screen.add_widget(self.calcium_concentration)
+        screen.add_widget(self.chloride_concentration)
+        screen.add_widget(self.sulphate_concentration)
+        screen.add_widget(self.alkalinity_as_carbonate_concentration)
+        screen.add_widget(self.sodium_concentration)
+        screen.add_widget(self.magnesium_concentration)
+        button = MDRectangleFlatButton(
+            text='Enter',
+            pos_hint={'center_x': 0.5, 'center_y': 0.2},
+            on_release=self.show_data)
+        screen.add_widget(button)
+        return screen
 
-    def quit(self):
-        App.get_running_app().stop()
-        exit()
+    def show_data(self, obj):
+        if self.calcium_concentration.text is "":
+            output_string = 'Please enter a calcium concentration'
+
+        water_profile = np.array([
+            float(self.calcium_concentration.text),
+            float(self.chloride_concentration.text),
+            float(self.sulphate_concentration.text),
+            float(self.alkalinity_as_carbonate_concentration.text),
+            float(self.sodium_concentration.text),
+            float(self.magnesium_concentration.text)
+        ])
+        calculated_profile = calculate_profile(
+            water_profile,
+            {
+                'ams': 1,
+                'calcium_chloride': 0,
+                'calcium_sulphate': 0,
+                'dwb': 0,
+                'magnesium_sulphate': 0,
+                'sodium_chloride': 1,
+                'lactic_acid': 0,
+            }
+        )
+
+        output_string = ''
+        output_string += 'Ca: ' + str(calculated_profile[0]) + '\n'
+        output_string += 'Cl: ' + str(calculated_profile[1]) + '\n'
+        output_string += 'SO4: ' + str(calculated_profile[2]) + '\n'
+        output_string += 'Alkalinity: ' + str(calculated_profile[3]) + '\n'
+        output_string += 'Na: ' + str(calculated_profile[4]) + '\n'
+        output_string += 'Mg: ' + str(calculated_profile[5]) + '\n'
+
+        close_button = MDFlatButton(text='Close', on_release=self.close_dialog)
+        more_button = MDFlatButton(text='More')
+        self.dialog = MDDialog(
+            text=output_string,
+            title='Titular Title',
+            size_hint=(0.7, 1),
+            buttons=[close_button, more_button])
+        self.dialog.open(self)
+
+    def close_dialog(self, obj):
+        self.dialog.dismiss(self)
 
 
-if __name__ == '__main__':
-    TestApp().run()
+DemoApp().run()
